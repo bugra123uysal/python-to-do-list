2876715d48303a52d4dfc6b2ead1f021ad72630f
buğra uysal <uysalbugra134@gmail.com>
2024-07-12 01:35:27 +0300

    yazılan görevleri ekranda  gösterme ve alt alta sıralama eklendi

--- tdlst.py
+++ tdlst.py
@@ -1,23 +1,40 @@
 
 import tkinter as tk
 pencere =tk.Tk()
-pencere.geometry("600x300")
+pencere.geometry("900x600")
 
+def göref_yap():
+    """ kullanıcının yazdığı metni alır """
+    kullanıc_al=kullanıcı.get()
+
+    """girilen yeni kelimeyi  alt alta yazdirmak """
+    mgor['text'] += '\n'+kullanıc_al
+
+    
 
 başlık=tk.Label(pencere,text="to-do list")
 başlık.pack()
 """ yapılacakları yazılacak yer"""
 kullanıcı=tk.Entry(pencere, width=30 )
 kullanıcı.pack()
+""" kullanıcının yazdığı metni alır """
+
 """ buttonlar """
-dugmea=tk.Button(pencere,text="ekle")
-dugmea.pack()
+""" buttona bastığında göref_yap çalışır """
+dugmea=tk.Button(pencere,text="ekle", command=göref_yap)
+dugmea.pack(pady=5)
 
 dugmeb=tk.Button(pencere,text="görevi tamamala" )
-dugmeb.pack()
+dugmeb.pack(pady=5)
 
 dugmec=tk.Button(pencere,text="sil")
-dugmec.pack()
+dugmec.pack(pady=5)
+
+""" metin göstermek için """
+mgor=tk.Label(pencere,text="")
+mgor.pack(pady=30, padx=30) 
+
+
 
 pencere.mainloop()
 
