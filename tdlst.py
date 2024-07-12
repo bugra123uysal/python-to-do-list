4abebe3347b52cdb4c88ab9d99f5fd015637b84e
buğra uysal <uysalbugra134@gmail.com>
2024-07-13 01:25:53 +0300

    ekle buttonuna basdıkca Checkbutton eklenecek, görevlerde yanında yazılı olucak

--- tdlst.py
+++ tdlst.py
@@ -4,16 +4,20 @@ pencere =tk.Tk()
 pencere.geometry("900x600")
 
 def göref_yap():
+
     """ kullanıcının yazdığı metni alır """
     kullanıc_al=kullanıcı.get()
-   
-  
+    """girilen yeni kelimeyi  alt alta yazdirmak 
+    mgor['text']+= '\n'+kullanıc_al
+    """
+    """ görev silmek """
+    var=tk.BooleanVar()
+    """ check ekleme """
+    checetm=tk.Checkbutton(pencere,text=kullanıc_al , variable=var)
+    checetm.pack()
 
-    """girilen yeni kelimeyi  alt alta yazdirmak """
-    mgor['text'] += '\n'+kullanıc_al
+    
 
-def konrtolet():
-     """ check box a basınca şü yazı çoksın """
 
 başlık=tk.Label(pencere,text="to-do list")
 başlık.pack()
@@ -24,7 +28,7 @@ kullanıcı.pack()
 
 """ buttonlar """
 """ buttona bastığında göref_yap çalışır """
-dugmea=tk.Button(pencere,text="ekle", command=göref_yap)
+dugmea=tk.Button(pencere,text="ekle", command=göref_yap )
 dugmea.pack(pady=5)
 
 dugmeb=tk.Button(pencere,text="görevi tamamala" )
@@ -38,10 +42,6 @@ mgor=tk.Label(pencere,text="")
 mgor.pack(pady=30, padx=30) 
 
 
-""" check ekleme """
-checetm=tk.Checkbutton(pencere)
-checetm.pack()
-
-
 pencere.mainloop()
+
 
